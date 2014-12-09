005dfc6ee117eff718b4199a9372edc823a88eb6
CS <cornelia.salomon@gmx.at>
2014-12-09 17:32:30 +0100

    GUI anpassung und Änderung der Amplitude von Mörti
    zusätzlich eine Function die den start und ende von Messzeiten holt vom
    Event

--- BMCA/Amplitude_Frequency_analysis.m
+++ BMCA/Amplitude_Frequency_analysis.m
@@ -1,36 +1,18 @@
 %ES WURDEN BISHER ALLE WERTE EINES CHANNELS MIT EINBEZOGEN (KEIN TRIGGER - WINDOWING)!
+function[RMS, peak_row, peak_col, Max_p_y,Max_p_x, Median_p, ben_y] = Amplitude_Frequency_analysis(channel, Messbeginn, Messende, Benutzerdefinierter_Zeitpunkt)
 
-clc
-load phoenix.mat
-start = 7;  %Ab diesem Datensatz wird ausgegeben
-num = 10;   %Bis zu diesem Datensatz wird ausgegeben
-figure(1);
-
-% x = find(data(1,:)>0);
-% starttrigger = min(x);
-% if (x ~= 0)
-    for foo = start:num  %%foo = 1:num
-        [peak_col,peak_row] = max(phoenix.data(foo,:)); %findet nicht immer den max-wert... 
-        RMS = sqrt(mean(phoenix.data(foo,:).^2));       %Root mean square eines ges. Kanals
+%Amplitude
+    [peak_col,peak_row] = max(channel(Messbeginn:Messende)); %findet nicht immer den max-wert... 
+    RMS = sqrt(mean(channel(Messbeginn:Messende).^2));       %Root mean square eines ges. Kanals
+    if(Benutzerdefinierter_Zeitpunkt==0)
+        ben_y = NaN;
+    else
+        ben_y = channel(str2num(Benutzerdefinierter_Zeitpunkt));
+    end
+   % subplot(num+1-start,2,foo+1-start);
         
-        subplot(num+1-start,2,foo+1-start);
-        hold on
-        plot(abs(phoenix.data(foo,:)));
-        plot(peak_row,peak_col,'r*');
-        plot(0:10:length(phoenix.data(foo,:)),RMS,'g-')
-        title(['RMS: ',num2str(RMS),'   Max. Amplitude, x: ',num2str(peak_row),' y:',num2str(peak_col)]) 
-        hold off
-    end
-    for foo = start:num  %%foo = 1:num
-        Power = abs(fft(phoenix.data(foo,:)));   %Powersprektrum der gewählten Kanäle
-        Median_p = median(Power(1,1:6700));      %Median von Power (1:6700 , weil sonst Spiegelung miteinbezogen wird=
-    
-        subplot(num+1-start,2,foo+start+1-num);
-        hold on
-        plot(Power);
-        plot(Median_p,0:500:30000,'r-')
-        hold off
-        title(['Median: ',num2str(Median_p)]) 
-        axis([0 6700 0 30000])
-    end
-% end
+ %Frequenz      
+    Power = abs(fft(channel));   %Powersprektrum der gewählten Kanäle
+    Median_p = median(Power(1,1:(length(Power)/2)));      %Median von Power (1:6700 , weil sonst Spiegelung miteinbezogen wird=
+    [Max_p_y Max_p_x] = max(Power(1,1:(length(Power)/2)));
+end
---
+++ BMCA/Messzeiten.m
@@ -0,0 +1,9 @@
+function[Messbeginn Messende] = Messzeiten (event)
+
+    x = find(event>0);
+    Messbeginn = min(x);
+    
+    x = find(event(Messbeginn:end)<0);
+    Messende =  min(x);
+    
+end
